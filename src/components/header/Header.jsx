3e50fcabfdd0985a308a0701bc537ef5561c1305
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-02 14:05:40 +0200

    Header styled using tailwind and regular css

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import logo from "../../assets/rick-morty.png";
+import "./header.css";
+function Header() {
+  return (
+    <div className="header">
+      <header className="myHeader container mx-auto max-h12 flex justify-around items-center py-2">
+        <img
+          className="rounded-full"
+          src={logo}
+          alt="Rick and Morty character logo"
+          height={80}
+          width={80}
+        />
+        <h1 className="text-3xl font-mono">
+          Rick and Morty character database
+        </h1>
+        <input
+          className="searchInput ring-teal-700 rounded-3xl text-center scale-125 placeholder-black placeholder:italic font-light"
+          type="search"
+          placeholder="Search a charatcer..."
+        />
+      </header>
+    </div>
+  );
+}
+
+export default Header;
