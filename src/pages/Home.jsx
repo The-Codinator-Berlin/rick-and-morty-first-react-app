067de125844176fd36260ccddd34ee300c31fb2c
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-03 20:04:19 +0200

    Footer import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 import CardGrid from "../components/card_grid/CardGrid";
 import Header from "../components/header/Header";
+import Footer from "../Footer";
+
 function Home() {
   return (
     <div>
       <Header />
       <CardGrid />
+      <Footer />
     </div>
   );
 }
 
-export default Home
+export default Home;
