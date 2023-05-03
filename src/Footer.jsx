7d6f71d68b3d8e44a80571d8053b17a37dd0d07a
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-03 19:38:18 +0200

    Footer added

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Pagination from "./Pagination";
+function Footer() {
+  return (
+    <div className="Pcontainer">
+      <Pagination />
+    </div>
+  );
+}
+
+export default Footer;
