4a615ec550e6ee71480b62413766ea75505ab211
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-03 20:00:46 +0200

    H5 added

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -13,6 +13,7 @@ export default function Example() {
           <span className="sr-only">Previous</span>
           <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
         </button>
+        <h5 className="text-xl text-black">Pages</h5>
         <button
           type="button"
           className="bg-lime relative -ml-px inline-flex items-center rounded-r-md bg-white px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
