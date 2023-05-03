e97c0f1c961a885c161ef5f20fe4753dd3bda65c
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-03 11:37:58 +0200

    Grid template extended

--- src/components/card_grid/CardGrid.jsx
+++ src/components/card_grid/CardGrid.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import "./CardGrid.css";
 function CardGrid() {
   return (
-    <div className="grid grid-cols-5 gap-4 pt-1 m-4	">
+    <div className="grid grid-cols-5 gap-4 pt-1 m-4">
       <div className="divGrid text-center">01</div>
       <div className="divGrid text-center">02</div>
       <div className="divGrid text-center">03</div>
       <div className="divGrid text-center">04</div>
       <div className="divGrid text-center">05</div>
+      <div className="divGrid text-center">06</div>
+      <div className="divGrid text-center">07</div>
+      <div className="divGrid text-center">08</div>
+      <div className="divGrid text-center">09</div>
+      <div className="divGrid text-center">10</div>
+      <div className="divGrid text-center">11</div>
+      <div className="divGrid text-center">12</div>
+      <div className="divGrid text-center">13</div>
+      <div className="divGrid text-center">14</div>
+      <div className="divGrid text-center">15</div>
+      <div className="divGrid text-center">16</div>
+      <div className="divGrid text-center">17</div>
+      <div className="divGrid text-center">18</div>
+      <div className="divGrid text-center">19</div>
+      <div className="divGrid text-center">20</div>
     </div>
   );
 }
