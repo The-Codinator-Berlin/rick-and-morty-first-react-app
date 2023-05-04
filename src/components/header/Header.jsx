02294c51b4b61278dad2ef580f1cd3c372d52e9a
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-04 13:47:32 +0200

    Started to implement search functionality

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import logo from "../../assets/rick-morty.png";
 import "./header.css";
-function Header() {
+function Header(props) {
+  console.log("props :>> ", props);
+  const getInputText = props.getInputText;
+  const inputText = "ricky";
+  //Option A
+  const sendText = () => {
+    getInputText(inputText);
+  };
   return (
     <div className="header">
       <header className="myHeader container mx-auto max-h12 flex justify-around items-center py-2">
@@ -18,7 +25,11 @@ function Header() {
         <input
           className="searchInput ring-teal-700 rounded-3xl text-center scale-125 placeholder-black placeholder:italic font-light"
           type="search"
-          placeholder="Search a charatcer..."
+          placeholder="Search a charatcer.."
+          // onChange={() => {
+          //   getInputText(inputText);
+          // }} //Option B
+          onChange={sendText} // Option A
         />
       </header>
     </div>
