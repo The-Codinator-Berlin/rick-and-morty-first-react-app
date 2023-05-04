10684d08f040c26d4553310520f3edab4602859d
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-04 13:48:28 +0200

    Commented out now as not being used in this way

--- src/components/card_grid/CardGrid.jsx
+++ src/components/card_grid/CardGrid.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import "./CardGrid.css";
-function CardGrid() {
-  return (
-    <div className="grid grid-cols-5 gap-4 pt-1 m-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-      <div className="rounded-lg divGrid text-center">01</div>
-      <div className="rounded-lg divGrid text-center">02</div>
-      <div className="rounded-lg divGrid text-center">03</div>
-      <div className="rounded-lg divGrid text-center">04</div>
-      <div className="rounded-lg divGrid text-center">05</div>
-      <div className="rounded-lg divGrid text-center">06</div>
-      <div className="rounded-lg divGrid text-center">07</div>
-      <div className="rounded-lg divGrid text-center">08</div>
-      <div className="rounded-lg divGrid text-center">09</div>
-      <div className="rounded-lg divGrid text-center">10</div>
-      <div className="rounded-lg divGrid text-center">11</div>
-      <div className="rounded-lg divGrid text-center">12</div>
-      <div className="rounded-lg divGrid text-center">13</div>
-      <div className="rounded-lg divGrid text-center">14</div>
-      <div className="rounded-lg divGrid text-center">15</div>
-      <div className="rounded-lg divGrid text-center">16</div>
-      <div className="rounded-lg divGrid text-center">17</div>
-      <div className="rounded-lg divGrid text-center">18</div>
-      <div className="rounded-lg divGrid text-center">19</div>
-      <div className="rounded-lg divGrid text-center">20</div>
-    </div>
-  );
-}
+// import React from "react";
+// import "./CardGrid.css";
+// function CardGrid() {
+//   return (
+//     <div className="grid grid-cols-5 gap-4 pt-1 m-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+//       <div className="rounded-lg divGrid text-center">01</div>
+//       <div className="rounded-lg divGrid text-center">02</div>
+//       <div className="rounded-lg divGrid text-center">03</div>
+//       <div className="rounded-lg divGrid text-center">04</div>
+//       <div className="rounded-lg divGrid text-center">05</div>
+//       <div className="rounded-lg divGrid text-center">06</div>
+//       <div className="rounded-lg divGrid text-center">07</div>
+//       <div className="rounded-lg divGrid text-center">08</div>
+//       <div className="rounded-lg divGrid text-center">09</div>
+//       <div className="rounded-lg divGrid text-center">10</div>
+//       <div className="rounded-lg divGrid text-center">11</div>
+//       <div className="rounded-lg divGrid text-center">12</div>
+//       <div className="rounded-lg divGrid text-center">13</div>
+//       <div className="rounded-lg divGrid text-center">14</div>
+//       <div className="rounded-lg divGrid text-center">15</div>
+//       <div className="rounded-lg divGrid text-center">16</div>
+//       <div className="rounded-lg divGrid text-center">17</div>
+//       <div className="rounded-lg divGrid text-center">18</div>
+//       <div className="rounded-lg divGrid text-center">19</div>
+//       <div className="rounded-lg divGrid text-center">20</div>
+//     </div>
+//   );
+// }
 
-export default CardGrid;
+// export default CardGrid;
